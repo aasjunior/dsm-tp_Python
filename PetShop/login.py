96814ee52f3e726938ad20b218c725122ff52386
aasjunior <adeldivo@hotmail.com>
2023-03-26 01:44:07 -0300

    25-03-2023

--- PetShop/login.py
+++ PetShop/login.py
@@ -1,30 +1,67 @@
+# Importação de Bibliotecas ---------------------------------------------------------------------------------------------
+
 from tkinter import *
+from customtkinter import *
 
-#Paleta de Cores -------------------------------------------
+# Paleta de Cores -------------------------------------------------------------------------------------------------------
 
-black = "#010409"
-gray = "#0d1117"
+from paleta_cores import *
 
-#Configuração de Tela --------------------------------------
+# Configuração de Tela --------------------------------------------------------------------------------------------------
 
-tela = Tk()
-tela.title("PetShop")
-tela.geometry("500x300")
+tela = CTk()
+tela.title("Login")
+tela.geometry("600x500")
 tela.resizable(False, False)
 
-#Frames ----------------------------------------------------
+tela._set_appearance_mode("dark")
+set_default_color_theme("green")
 
-wrapper = Frame(tela, bg=black)
-wrapper.pack(fill='both', expand=True)
-container = Frame(wrapper, width=300, height=300, bg=gray)
-container.place(relx=0.5, rely=0.5, anchor=CENTER)
+# Formulário -----------------------------------------------------------------------------------------------------------
 
-#Campos ----------------------------------------------------
+formLogin = CTkFrame(tela, corner_radius=20)
+formFieldset = CTkFrame(formLogin, fg_color="transparent")
+formBtns = CTkFrame(formFieldset, fg_color="transparent")
 
-lbl_usuario = Label(container, text="Usuário").pack()
-txt_usuario = Entry(container, width=20).pack()
+legend = CTkLabel(formFieldset, text="Fazer Login", font=("arial bold", 16))
 
-lbl_senha = Label(container, text="Senha").pack()
-txt_senha = Entry(container, width=20).pack()
+txtEmail = CTkEntry(formFieldset, placeholder_text="Seu E-mail", width=210)
+txtSenha = CTkEntry(formFieldset, placeholder_text="Sua Senha", width=210)
+
+btnLogin = CTkButton(formBtns, text="Login", fg_color=green, width=100)
+btnRegistrar = CTkButton(formBtns, text="Registrar", fg_color="transparent", border_width=2, border_color="gray", width=100)
+btnEsqueceuSenha = CTkLabel(formBtns, text="Esqueceu a Senha")
+
+# Configurando os Widgets --------------------------------------------------------------------------------------------
+
+# Cursor Pointer
+
+btnEsqueceuSenha.configure(cursor="hand2")
+
+# Adicionando efeito hover ao botão
+
+def on_enter(e):
+    btnEsqueceuSenha.configure(text_color="light_green")
+def on_leave(e):
+    btnEsqueceuSenha.configure(text_color="white")
+
+# Associando evento Enter e Leave ao botão
+
+btnEsqueceuSenha.bind("<Enter>", on_enter)
+btnEsqueceuSenha.bind("<Leave>", on_leave)
+
+# Gerenciadores
+
+formLogin.pack(padx=150, pady=110, fill=BOTH, expand=TRUE)
+formFieldset.place(relx=0.5, rely=0.5, anchor=CENTER)
+legend.pack(pady=(10, 20))
+txtEmail.pack(pady=(0, 10))
+txtSenha.pack(pady=(0, 10))
+formBtns.pack(pady=(10, 10))
+btnEsqueceuSenha.pack(pady=(5,0), side=BOTTOM)
+btnLogin.pack(padx=(0, 5), pady=(0, 5), side=LEFT)
+btnRegistrar.pack(padx=(5, 0), pady=(0, 5), side=RIGHT)
+
+# Fim -------------------------------------------------------------------------------------------------------------------
 
 tela.mainloop()
